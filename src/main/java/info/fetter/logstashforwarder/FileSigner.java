708c05d931cbf1530a3faead0dbe0c13c0c0d640
didfet <didfet@free.fr>
2015-03-12 19:22:29 +0100

    Implemented FileSigner.

---
+++ src/main/java/info/fetter/logstashforwarder/FileSigner.java
@@ -0,0 +1,18 @@
+package info.fetter.logstashforwarder;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.zip.Adler32;
+
+public class FileSigner {
+	private static final Adler32 adler32 = new Adler32();
+	
+	public static long computeSignature(RandomAccessFile file, int signatureLength) throws IOException {
+		adler32.reset();
+		byte[] input = new byte[signatureLength];
+		file.seek(0);
+		file.read(input);
+		adler32.update(input);
+		return adler32.getValue();
+	}
+}
